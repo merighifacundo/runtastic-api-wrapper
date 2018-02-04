2d815ca86611735c31c7f37f4c00a7710249c06e
Facundo Merighi <merighifacundo@gmail.com>
2018-02-03 23:39:49 -0500

    Adding cross domain headers

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,10 @@ module MyrunhubApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.active_job.queue_adapter = :sucker_punch
+
+    config.action_dispatch.default_headers = {
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
